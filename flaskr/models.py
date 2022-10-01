3e374b936a762bec6b3eb7284caa9dd3afef6448
Remy349 <santoraga15@gmail.com>
2022-09-30 23:16:28 -0600

    Add image upload form and connection to cloudinary

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,2 +1,12 @@
 from flaskr import db
 
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"""
+            Image:
+                id: {self.id}
+        """
+
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,8 +1,34 @@
-from flask import render_template
+from flask import flash, redirect, render_template, request, url_for
+from cloudinary.uploader import upload
 from flaskr import app
+
+ALLOWED_EXTENSIONS = {"png", "jpg", "gif", "jpeg"}
+
+
+def allowed_file(filename):
+    return "." in filename and \
+        filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.route("/add_image", methods=["POST"])
+def add_image():
+    file = request.files["file"]
+
+    if file.filename == "":
+        flash("No selected file!")
+        return redirect(url_for("index"))
+
+    if file and allowed_file(file.filename):
+        upload_result = upload(file)
+        print(upload_result)
+    else:
+        flash("File type not allowed!")
+        return redirect(url_for("index"))
+
+    return redirect(url_for("index"))
 
